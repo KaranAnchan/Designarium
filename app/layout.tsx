deb2b6847426c35fbf08b393a42959d8ca0441c8
Karan Anchan <kar.anchan02@gmail.com>
2023-06-28 16:01:51 +0530

    Structure And Footer Built

---
+++ app/layout.tsx
@@ -0,0 +1,28 @@
+import favicon from "./favicon.ico";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export const metadata = {
+  title: "Designarium",
+  description: "Built By Karan Anchan",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href={favicon.src} />
+      </head>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
